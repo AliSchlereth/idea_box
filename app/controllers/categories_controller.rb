dad9b3c9bd88d1ee9c8f621112ec4256917ac58f
Ali Schlereth <aschlere@sbcglobal.net>
2016-10-25 21:03:32 -0600

    Complete CRUD Categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -21,10 +21,23 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update_attributes(category_params)
+      redirect_to category_path(@category)
+    else
+      render :edit
+    end
+  end
+
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
-    
+
     redirect_to categories_path
   end
 
