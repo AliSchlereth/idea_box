0829772fda613d89929070b276a0001146122125
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-16 20:50:00 -0700

    Add user image index and navigation links

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -5,6 +5,10 @@ class ImagesController < ApplicationController
     @image = Image.find(params[:id])
   end
 
+  def index
+    @images = Image.all
+  end
+
   private
   def image_params
     params.require(:image).permit(:url)
