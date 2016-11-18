d2184799b602b79223ff13ab1f80cfc5ff96eaa0
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-17 22:19:42 -0700

    Refactor Ideas specs

--- spec/features/ideas/user_sees_all_ideas_spec.rb
+++ spec/features/ideas/user_sees_all_ideas_spec.rb
@@ -1,26 +1,25 @@
 require 'rails_helper'
 
 describe "User visits the idea index" do
+  before :each do
+    @user = create(:user)
+    @cat = create(:category)
+    @idea1 = @user.ideas.create(name: "Name1", category: @cat)
+  end
   scenario "they see all the current ideas" do
-    user = create(:user)
-    cat = create(:category)
-    idea1 = user.ideas.create(name: "Name1", category: cat)
-    idea2 = user.ideas.create(name: "Name2", category: cat)
+    idea2 = @user.ideas.create(name: "Name2", category: @cat)
 
-    visit user_ideas_path(user)
+    visit user_ideas_path(@user)
 
-    expect(page).to have_content idea1.name
+    expect(page).to have_content @idea1.name
     expect(page).to have_content idea2.name
   end
 
   scenario "they can click each idea to redirect to idea show" do
-    user = create(:user)
-    cat = create(:category)
-    idea = user.ideas.create(name: "Name", category: cat)
 
-    visit user_ideas_path(user)
+    visit user_ideas_path(@user)
     click_on "Name"
 
-    expect(current_path).to eq(user_idea_path(user, idea))
+    expect(current_path).to eq(user_idea_path(@user, @idea1))
   end
 end
