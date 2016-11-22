dc26f2c74bc49f86a62721612c86f46749ead70e
Ali Schlereth <aschlere@sbcglobal.net>
2016-11-21 21:48:41 -0700

    Initialize seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+class SeedData
+
+  def create_images
+    puts "creating images"
+    Image.create!(url: "http://www.hrc.org/files/images/welcoming_schools/general/Mixed_race_family_600.jpg")
+    puts "created images"
+  end
+end
+
+seed = SeedData.new
+seed.create_images
